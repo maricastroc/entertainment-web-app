7459055b043ae1cd77dedce239a185720db9fcd9
mariana castro <marianacastrorc@gmail.com>
2025-05-16 21:14:25 -0300

    refactoring

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,15 @@ module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1', // Mantém os aliases de importação
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
-  // Novo padrão para encontrar testes dentro de src
   testMatch: ['<rootDir>/src/**/*.test.(ts|tsx)'],
   
-  // Opcional: ignora pastas de build e node_modules
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/dist/'
   ],
   
-  // Configurações adicionais recomendadas
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
